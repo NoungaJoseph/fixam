5d313d658d074eb1450b131acb62ecd62f304830
NoungaJoseph <noungajoseph58@gmail.com>
2026-05-27 13:28:48 +0100

    fix: remove watcher.unstable_workerThreads from metro config

--- app/metro.config.js
+++ app/metro.config.js
@@ -4,4 +4,7 @@ const { getDefaultConfig } = require('@expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// EAS validation fails on this property, so we must remove it from the default config
+delete config.watcher.unstable_workerThreads;
+
 module.exports = config;
